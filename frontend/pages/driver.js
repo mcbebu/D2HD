cc932281d738ec0a4978b0f7c2e289583deab91b
Elroy Chua <elroymx@gmail.com>
2023-02-25 20:46:27 +0800

    merged driver frontend chages

--- frontend/pages/driver.js
+++ frontend/pages/driver.js
@@ -1,35 +1,63 @@
-import { Flex, ListItem, ListIcon, Button, List } from "@chakra-ui/react";
+import { Flex, ListItem, ListIcon, Button, List, Text } from "@chakra-ui/react";
 import { useState } from 'react';
 import { MdCheckCircle, MdCheckCircleOutline } from "react-icons/md";
+import { RxCrossCircled } from "react-icons/rx";
 import React from "react";
 
 const consigneeDetails = [
-  { address: "tampines blk 1", is_completed: "false" },
-  { address: "tampines blk 2", is_completed: "false" },
-  { address: "tampines blk 3", is_completed: "false" },
+  { address: 'tampines blk 0', is_completed: false },
+  { address: 'tampines blk 1', is_completed: false },
+  { address: 'tampines blk 2', is_completed: false },
+  { address: 'tampines blk 3', is_completed: false },
+  { address: 'tampines blk 4', is_completed: false },
+  { address: 'tampines blk 5', is_completed: false },
 ];
 
 
 const Driver = () => {
-  // const handleDelivery = () => {
-  //   onDelivery();
-  // };
-  const [ isParcelDelivered, setParcelDelivered ] = useState(false);
+  const [ consignees, setConsignees ] = useState(consigneeDetails);
+
+  const handleAccepted = (index) => {
+    const updatedConsignees = [ ...consignees ];
+    updatedConsignees[ index ].is_completed = true;
+    setConsignees(updatedConsignees);
+  };
+
+  const handleRejected = (index) => {
+    const updatedConsignees = [ ...consignees ];
+    updatedConsignees[ index ].is_rejected = true;
+    setConsignees(updatedConsignees);
+  };
 
   return (
-    <Flex alignContent='center'>
-      <List spacing={3}>
-        <ListItem>
-          <ListIcon as={false ? MdCheckCircle : MdCheckCircleOutline} color='green.500' />
-          Package 1
-        </ListItem>
-      </List>
-      <Button
-        onClick={() =>
-          setParcelDelivered((is_completed) => !is_completed)
-        }>
-      </Button>
-    </Flex>
+    <Flex minWidth="100%"
+      direction="column">
+      <Navbar />
+      <Flex className="Driver List" minWidth="100%" direction="column" alignSelf='center'>
+        <List spacing={3}>
+          {consigneeDetails.slice(0,5).map((consignee, index) => (
+            <ListItem key={index}>
+              <ListIcon
+              as={consignee.is_completed ? MdCheckCircle : consignee.is_rejected ? RxCrossCircled : MdCheckCircleOutline}
+                color={consignee.is_completed ? 'green.500' : (consignee.is_rejected  ? 'red.500' : 'gray.500')}
+                boxSize={10}
+              />
+              {consignee.address}
+              <Button
+                isDisabled={consignee.is_rejected || consignee.is_completed}
+                onClick={() => handleAccepted(index)}>
+                {consignee.is_completed ? "Accepted" : "Accept"}
+              </Button>
+              <Button
+                isDisabled={consignee.is_completed || consignee.is_rejected}
+                onClick={() => handleRejected(index)}>
+                {consignee.is_rejected ? "Rejected" : "Reject"}
+              </Button>
+            </ListItem>
+          ))}
+        </List>
+      </Flex>
+    </Flex >
   );
 };
 export default Driver;
