fbc4051e70f2cb48573a748d937c9f5eba8e7ecf
Sean Marinas <seanmarinas@gmail.com>
2023-02-25 12:18:25 +0800

    added file structure for backend

---
+++ backend/src/main/java/d2hd/hackathon/codedojobackend/CodeDojoBackendApplication.java
@@ -0,0 +1,13 @@
+package d2hd.hackathon.codedojobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeDojoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeDojoBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/d2hd/hackathon/codedojobackend/CodeDojoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package d2hd.hackathon.codedojobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeDojoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
