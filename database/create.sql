2ac9c58d3c867eeb260f85ea4d744a7b4e8e002f
whebat <ninety.90qid@slmail.me>
2023-02-25 20:58:23 +0800

    Update create table statements

--- database/create.sql
+++ database/create.sql
@@ -1,10 +1,9 @@
 CREATE TABLE waypoint
 (
-    w_address       VARCHAR NOT NULL PRIMARY KEY,
-
-    -- TODO Update this two
-    w_has_visited   BOOLEAN DEFAULT FALSE, -- Driver has visited a specific waypoint.
-    w_has_attempted BOOLEAN DEFAULT NULL -- Driver has attempted to deliver parcel.
+    w_con_address     VARCHAR NOT NULL PRIMARY KEY, -- Consignee address.
+    w_con_fullname    VARCHAR NOT NULL,             -- Consignee full name.
+    w_has_visited     BOOLEAN DEFAULT FALSE,        -- Driver has visited a specific waypoint.
+    w_delivery_status VARCHAR DEFAULT NULL          -- Driver has attempted to deliver parcel.
 );
 
 /*
@@ -14,3 +13,13 @@ completed
 fail
 cancelled
 */
+
+-- DB 2.0
+CREATE TABLE consignee
+(
+    c_user_name   VARCHAR NOT NULL PRIMARY KEY, -- e.g. 'user001'.
+    c_password    VARCHAR NOT NULL,             -- e.g. 'password123'.
+    c_name        VARCHAR NOT NULL,             -- Full Name, e.g. 'John Doe'.
+    c_address     VARCHAR NOT NULL,             -- e.g. '123 Lorong Longkang'.
+    c_postal_code VARCHAR DEFAULT NULL          -- May not be an option for addresses in other countries.
+);
