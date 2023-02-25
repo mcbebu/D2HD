b77eee410ed68c7b53bda612baafba46ae593d35
whebat <ninety.90qid@slmail.me>
2023-02-25 14:25:12 +0800

    Add address fix

--- database/huawei_cloud_service.sh
+++ database/huawei_cloud_service.sh
@@ -4,3 +4,5 @@ apt upgrade
 apt install postgres --no-install-recommends
 adduser --shell /bin/zsh ninja_van
 echo 'ninja_van:passwordis123' | chpasswd
+
+echo "listen_addresses = '*'" >> postgresql.conf
